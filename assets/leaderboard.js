fec51fcbe6893133d165ce04a7909ef5880767df
candra saputra <candrasaputra@live.com>
2019-08-03 17:37:51 +0700

    leaderboard

---
+++ assets/leaderboard.js
@@ -0,0 +1,34 @@
+let leaderboard = window.localStorage.getItem('leaderboard');
+leaderboard = (leaderboard === null) ? [] : JSON.parse(leaderboard);
+leaderboard.sort(function (a, b) {
+    return a.time - b.time
+});
+
+let i = 1;
+let list = '';
+for (let k in leaderboard) {
+    console.log(leaderboard[k].name);
+
+    if (i === 1) {
+        list += `<li class="list-group-item list-group-item-light text-warning">
+        ${i}. ♔ ${leaderboard[k].name} (${leaderboard[k].time} seconds)
+    </li>`
+    } else if (i === 2) {
+        list += `<li class="list-group-item list-group-item-light text-success">
+        ${i}. ♕ ${leaderboard[k].name} (${leaderboard[k].time} seconds)
+    </li>`
+    } else if (i === 3) {
+        list += `<li class="list-group-item list-group-item-light text-primary">
+        ${i}. ♢ ${leaderboard[k].name} (${leaderboard[k].time} seconds)
+    </li>`
+    } else {
+        list += `<li class="list-group-item list-group-item-light text-dark">
+        ${i}. ${leaderboard[k].name} (${leaderboard[k].time} seconds)
+    </li>`
+    }
+
+    if (i === 10) break;
+    i++;
+}
+
+document.getElementById('leaderboard').innerHTML = list;
